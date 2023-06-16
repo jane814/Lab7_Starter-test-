5f9e7bd3b48de2b10f40edeeaf9225a08cd54431
jane814 <63211137+jane814@users.noreply.github.com>
2023-06-16 14:21:00 -0700

    parsing

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -82,7 +82,7 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
-  var recipes = localStorage.getItem('recipes');
+  var recipes = JSON.parse(localStorage.getItem('recipes'));
   if (recipes != null){
     return recipes;
   }
